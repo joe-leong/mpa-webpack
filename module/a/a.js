b0bfa5edf5d35cf21e9b737f799d25c8469986eb
joe <zdnf_lgq@163.com>
2020-09-20 21:03:22 +0800

    feat: css loader

--- module/a/a.js
+++ module/a/a.js
@@ -1,1 +1,3 @@
+import '../../assets/index.css'
+
 console.log('aaaa')
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,34 @@
-const path = require('path')
-const fs = require('fs')
-const modules = fs.readdirSync(path.resolve(__dirname,'./module'))
-const HtmlWebpcKPlugin = require('html-webpack-plugin')
-const entries = {}
-const htmlPs = []
-modules.forEach(module=>{
-    entries[`${module}/${module}`] = `./module/${module}/${module}.js`,
-    htmlPs.push(new HtmlWebpcKPlugin({
-        filename:`${module}/${module}.html`,
-        template:`./module/${module}/index.html`,
-        chunks:[`${module}/${module}`]
-    }))
-})
+const path = require("path");
+const fs = require("fs");
+const modules = fs.readdirSync(path.resolve(__dirname, "./module"));
+const HtmlWebpcKPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const entries = {};
+const htmlPs = [];
+modules.forEach((module) => {
+  (entries[`${module}/${module}`] = `./module/${module}/${module}.js`),
+    htmlPs.push(
+      new HtmlWebpcKPlugin({
+        filename: `${module}/${module}.html`,
+        template: `./module/${module}/index.html`,
+        chunks: [`${module}/${module}`],
+      })
+    );
+});
 module.exports = {
-    mode:'development',
-    entry:entries,
-    output:{
-        path:path.resolve(__dirname,'dist'),
-        filename:'[name]-[hash:8].js'
-    },
-    plugins:[...htmlPs]
-}
+  mode: "development",
+  entry: entries,
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name]-[hash:8].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["file-loader", "css-loader", "postcss-loader"],
+      },
+    ],
+  },
+  plugins: [...htmlPs, new CleanWebpackPlugin()],
+};
